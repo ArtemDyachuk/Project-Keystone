c78348230ed0b78f4177f34f71436e61d1491c2f
Artem <Tommy.cardello@icloud.com>
2025-08-11 17:52:34 -0500

    ...

--- apps/front-ends/cms/next.config.js
+++ apps/front-ends/cms/next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['@keystone/database', '@keystone/ui'],
   outputFileTracingRoot: path.join(__dirname, "../../../"),
 };
 
