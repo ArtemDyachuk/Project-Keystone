494d346a6f8ace234510d46afc8934d3d44315d7
Artem <Tommy.cardello@icloud.com>
2025-08-13 01:48:07 -0500

    fix

--- apps/front-ends/cms/app/(auth)/login/LoginForm.tsx
+++ apps/front-ends/cms/app/(auth)/login/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "./login.module.css";
 import Link from "next/link";
