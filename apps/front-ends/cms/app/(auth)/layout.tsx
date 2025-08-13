a7d3575bebb8ce3c0e882208198070eea5dd81a9
Artem Dyachuk <a.dyachuk@icloud.com>
2025-08-13 00:53:30 -0500

    Update layout.tsx

--- apps/front-ends/cms/app/(auth)/layout.tsx
+++ apps/front-ends/cms/app/(auth)/layout.tsx
@@ -5,11 +5,5 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
